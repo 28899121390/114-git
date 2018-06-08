0584bd3407b7d5b2a485d0622c11629bcc9048e1
htf <2889921390@qq.com>
2018-06-08 15:09:18 +0800

    分支第一次提交

---
+++ src/main/java/com/baizhi/branch/Test.java
@@ -0,0 +1,10 @@
+package com.baizhi.branch;
+
+/**
+ * Created by Administrator on 2018/6/8.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我是分支");
+    }
+}
